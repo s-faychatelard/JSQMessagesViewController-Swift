cf7570876b9052fc984655f5f82648861e22cfea
Sylvain FAY-CHATELARD <sylvain.faychatelard@gmail.com>
2015-08-24 09:54:32 +0200

    JSQSystemSoundPlayer is now an intern dependency

--- JSQMessagesViewController/JSQMessagesViewController/JSQMessagesViewController.h
+++ JSQMessagesViewController/JSQMessagesViewController/JSQMessagesViewController.h
@@ -15,3 +15,4 @@ FOUNDATION_EXPORT double JSQMessagesViewControllerVersionNumber;
 FOUNDATION_EXPORT const unsigned char JSQMessagesViewControllerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesViewController/PublicHeader.h>
+#import <JSQSystemSoundPlayer/SystemSoundHelper.h>
